ac0dddc243119dd57eb963e16b9326ef12fea04e
Amit Kulkarni <yuvraj2987@gmail.com>
2014-04-25 18:17:40 -0400

    contactDls now has only dls server string

--- src/contactDls.py
+++ src/contactDls.py
@@ -5,14 +5,14 @@ import logging
 class ContactDls:
     ' Class to communicate with Directory Listing Service'
 
-    def __init__(self, dlsUrl, serverUrl):
+    def __init__(self, dlsUrl):
         self.dls = dlsUrl
-        self.remoteServer = serverUrl
+        #self.remoteServer = serverUrl
 
     def get_responce(self, path):
         logging.debug("---- get_responce starts -----")
         logging.debug("Passed path:%s", path)
-        path = self.remoteServer+path
+        #path = self.remoteServer+path
         logging.debug("Complete path:%s", path)
         payload = {"URI":path}
         http_responce = requests.get(self.dls, params=payload)
@@ -22,10 +22,11 @@ class ContactDls:
 
 def main():
     print "Testing ContactDls class"
-    dlsUrl = "http://ec2-184-73-223-158.compute-1.amazonaws.com:8080/DirectoryListingService/rest/dls/list"
+    #dlsUrl = "http://ec2-184-73-223-158.compute-1.amazonaws.com:8080/DirectoryListingService/rest/dls/list"
+    dlsUrl = "http://didclab-ws8.cse.buffalo.edu:8080/DirectoryListingService/rest/dls/list"
     remoteServer = "ftp://ftp.freebsd.org"
-    dlsClient = ContactDls(dlsUrl, remoteServer)
-    jsonResponce = dlsClient.get_responce("")
+    dlsClient = ContactDls(dlsUrl)
+    jsonResponce = dlsClient.get_responce(remoteServer)
     print "json responce\n", jsonResponce
     print "-----------------------"
     print "json key and values"
