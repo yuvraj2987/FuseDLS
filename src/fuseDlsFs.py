f73538c9111d0d77477d00d8be1d72438635c7ff
Amit Kulkarni <yuvraj2987@gmail.com>
2014-04-27 11:44:05 -0400

    Removed ro=true from FUSE call

--- src/fuseDlsFs.py
+++ src/fuseDlsFs.py
@@ -7,10 +7,33 @@ from time import time
 import logging
 from fuse import FUSE, FuseOSError, Operations, fuse_get_context
 
+from contactDls import json_to_dict, ContactDls 
+import cacheDls
 
 class FuseDLS(Operations):
     'FuseDLS for exporting Directory Listing Services a.k.a Stork Cloud Cache to Client as File System'
-
+    
+    def __init__(self, root):
+        logging.info("----- Init FuseDLS FS---")
+        self.root = root
+        self.curpath = "/"
+        dlsUrl = "http://didclab-ws8.cse.buffalo.edu:8080/DirectoryListingService/rest/dls/list"
+        self.dls = ContactDls(dlsUrl)
+        logging.debug("ContactDLS created")
+        self.cache = cacheDls.Cache(self.dls.get_responce)
+        logging.debug("Cache created")
+        logging.debug("Mounting the DLS cache")
+        mountResponce = self.dls.do_mount()
+        fileList = mountResponce.get("files")
+        for f in fileList:
+            val = json_to_dict(f)
+            key = val.get("name")
+            self.cache.add(key, val)
+        logging.debug("DLS cache mounted")
+        logging.info("--------- FuseDLS Initialized ----------")
+        
+    # FileSystem Methods
+    # -----------------------
     def getattr(self, path, fh=None):
         uid, gid, pid = fuse_get_context()
         if path == '/':
@@ -45,8 +68,8 @@ class FuseDLS(Operations):
 if __name__ == '__main__':
     
     print "--- FuseDLS FileSystem started ----"
-    logging.basicConfig(filename="log/test.log", level=logging.DEBUG)
+    logging.basicConfig(filename="log/test.log", filemode= "w", level=logging.DEBUG)
     logging.info("------------ FuseDLS Starts ---------")
     mountpoint = "/fuse_mount/dls"
     logging.info("mountpoint: %s ", mountpoint)
-    fuse = FUSE(FuseDLS(), mountpoint, foreground=False, ro=True)
+    fuse = FUSE(FuseDLS(mountpoint), mountpoint, foreground=False)
